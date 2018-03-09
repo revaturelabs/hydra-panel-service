266ab8bd2f1b3ca57442d7b20d5b263d9f1a6303
Matthew Little <mlittl9199@gmail.com>
2018-03-09 15:52:18 -0500

    removed some commented code and fixed a bug regarding package naming

--- src/main/java/com/revature/hydra/panel/PanelRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/panel/PanelRepositoryServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-//@EnableSwagger2
 @SpringBootApplication
-//@EnableEurekaClient
 //@ComponentScan({"com.revature.hydra.panel.config", "com.revature.hydra.panel.service"})
 @EntityScan("com.revature.beans")
 public class PanelRepositoryServiceApplication {
@@ -14,13 +12,4 @@ public class PanelRepositoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PanelRepositoryServiceApplication.class, args);
 	}
-	
-//	@Bean
-//    public Docket api() { 
-//        return new Docket(DocumentationType.SWAGGER_2)  
-//          .select()                                  
-//          .apis(RequestHandlerSelectors.basePackage("com.revature.hydra.panel.controller"))              
-//          .paths(PathSelectors.any())                          
-//          .build();
-//    }
 }
